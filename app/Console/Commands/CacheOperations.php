4f91fad34f3d3273ea2cdba082362b9938f9802b
thrasos <thr.bark@gmail.com>
2021-03-05 20:20:07 +0200

    Introduce command to clear redis

---
+++ app/Console/Commands/CacheOperations.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Redis;
+
+class CacheOperations extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'redis:clear';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Various operations for Redis';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        Redis::flushAll();
+        $this->info('Cache cleared!');
+    }
+}
